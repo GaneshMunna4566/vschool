27bcd842052e58b55f317b7e7dc3b67e1d57f809
GaneshMunna4566 <71213041+GaneshMunna4566@users.noreply.github.com>
2024-01-29 10:00:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import {BrowserRouter as Routers,Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landingpage from "./components/auth/Landingpage";
 import Signup from './components/auth/Signup';
 import Login from "./components/auth/Login";
 import Forgotpassword from "./components/auth/Forgotpassword";
 import ResetPassword from "./components/auth/ResetPassword";
 import UserProfile from "./components/auth/UserProfile";
-
 import Homepage from "./components/homepages/Homepage";
 import Add_member from "./components/homepages/Add_member";
 import ClassList from "./components/homepages/ClassList";
@@ -16,22 +15,22 @@ import CreateTaskForm from "./components/homepages/CreateTaskForm";
 function App() {
   return (
     <div className="App">
-      <Routers>
+      <Router>
         <Routes>
-          <Route path='/' Component={Landingpage}/>
-          <Route path='/signup' Component={Signup}/>
-          <Route path='/login' Component={Login}/>
-          <Route path='/forgotpassword' Component={Forgotpassword}/>
-          <Route path='/resetpassword' Component={ResetPassword}/>
-          <Route path='/profile' Component={UserProfile}/>
-          <Route path='/homepage' Component={Homepage}/>
-          <Route path='/add-member' Component={Add_member}/>
-          <Route path='/classes' Component={ClassList}/>
-          <Route path='/enroll' Component={EnrollmentsList}/>
-          <Route path='/fileupload' Component={FileUpload}/>
-          <Route path='/createtask' Component={CreateTaskForm}/>
+          <Route path='/' component={Landingpage}/>
+          <Route path='/signup' component={Signup}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/forgotpassword' component={Forgotpassword}/>
+          <Route path='/resetpassword' component={ResetPassword}/>
+          <Route path='/profile' component={UserProfile}/>
+          <Route path='/homepage' component={Homepage}/>
+          <Route path='/add-member' component={Add_member}/>
+          <Route path='/classes' component={ClassList}/>
+          <Route path='/enroll' component={EnrollmentsList}/>
+          <Route path='/fileupload' component={FileUpload}/>
+          <Route path='/createtask' component={CreateTaskForm}/>
         </Routes>
-      </Routers>
+      </Router>
     </div>
   );
 }
